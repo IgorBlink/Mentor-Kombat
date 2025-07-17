8b87989e503b5ca9c6c0f5cd61ac3e916a7ea13f
Azekowka <gabjtov@gmail.com>
2025-07-18 02:45:46 +0500

    contributors

--- mentorkombat/app/page.tsx
+++ mentorkombat/app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 import Image from "next/image"
+import { Demo as Avatars } from "@/components/avatars"
 
 export default function IntroScreen() {
   const router = useRouter()
@@ -34,6 +35,11 @@ export default function IntroScreen() {
           className="object-cover pixelated"
           priority
         />
+      </div>
+
+      {/* Top right corner avatars */}
+      <div className="absolute top-12 right-4 z-20" style={{ position: 'fixed', top: '3rem', right: '1rem', zIndex: 20 }}>
+        <Avatars />
       </div>
 
       <div className="relative z-10 flex flex-col h-screen justify-center items-center py-8">
---
+++ mentorkombat/components/avatars.tsx
@@ -0,0 +1,26 @@
+import { AvatarGroup } from "@/components/ui/avatar-group"
+
+const Demo = () => {
+    return (
+        <>
+            <AvatarGroup
+                avatars={[
+                {
+                    src: "https://avatars.githubusercontent.com/u/178751420?v=4",
+                    label: "Aziz Gabitov",
+                    url: "https://github.com/azekowka",
+                },
+                {
+                    src: "https://avatars.githubusercontent.com/u/165867923?v=4",
+                    label: "Igor Blink",
+                    url: "https://github.com/igorblink",
+                },
+                ]}
+                maxVisible={4}
+                size={45}
+            />
+        </>
+    )
+}
+
+export {Demo}
