beb8e61050b2cbd03f5afc8d5fbe5a818c8e1f9f
Azekowka <gabjtov@gmail.com>
2025-07-18 10:18:21 +0500

    favicon

--- mentorkombat/app/manifest.ts
+++ mentorkombat/app/manifest.ts
@@ -2,23 +2,23 @@ import type { MetadataRoute } from "next"
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Friedrichshain Connection",
-    short_name: "F-Hain Connection",
-    description: "A 90s-style fighting game set in Berlin",
+    name: "nFactorial Mentor Kombat",
+    short_name: "Mentor Kombat",
+    description: "Aziz & Igor",
     start_url: "/",
     display: "standalone",
     background_color: "#000000",
     theme_color: "#ff6b00",
     icons: [
       {
-        src: "/icons/icon-192x192.png",
+        src: "/icons/nfacfavicon.jpg",
         sizes: "192x192",
-        type: "image/png",
+        type: "image/jpeg",
       },
       {
-        src: "/icons/icon-512x512.png",
+        src: "/icons/nfacfavicon.jpg",
         sizes: "512x512",
-        type: "image/png",
+        type: "image/jpeg",
       },
     ],
   }
